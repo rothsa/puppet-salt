1dda3e6d833f039bfe43c3eb81d2a7a8ca3eb63f
Sally Lehman <rothsa@users.noreply.github.com>
2016-02-02 22:17:35 -0700

    remove obsolete anchor fix

--- manifests/minion.pp
+++ manifests/minion.pp
@@ -94,14 +94,4 @@ inherits salt::params {
   include 'salt::minion::config'
   include 'salt::minion::service'
 
-  # Anchor this as per #8140 - this ensures that classes won't float off and
-  # mess everything up.  You can read about this at:
-  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
-  anchor { 'salt::minion::begin': }
-
-  anchor { 'salt::minion::end': }
-
-  Anchor['salt::minion::begin'] -> Class['::salt::minion::install'] -> Class['::salt::minion::config'
-    ] ~> Class['::salt::minion::service'] -> Anchor['salt::minion::end']
-
 }
